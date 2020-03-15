0cf2ff3d9be4e198b42c7984131d6a30e6107f3e
tomomi kimura <caramel.maple509@gmail.com>
2020-03-15 13:28:14 +0000

    0315

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy,:edit,:show]
     def index
       #session[:user_id] = nil
       if logged_in?

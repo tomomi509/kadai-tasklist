df92e75e7606cb2ce2e5bb87bc71819e1a9318a4
tomomi kimura <caramel.maple509@gmail.com>
2020-03-21 01:07:08 +0000

    0320

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
     get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :show, :create]
 
-  #resources :tasklists, only: [:create, :destroy]
 end
